3f7e9cdfe91d772cb72d3d55086831039cc6f805
shyalandran <128975149+shyalandran@users.noreply.github.com>
2023-08-24 11:28:50 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector(".Starbucks").src = anything;
+}
+
+function circleColor(color) {
+    const circle = document.querySelector(".circle");
+    circle.style.background = color;
+}
